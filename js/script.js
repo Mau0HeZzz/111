a3ed68ce15929c7709081909255810aeb4b67cc7
Mau0HeZzz <mau0hezzz@hotmail.com>
2020-05-06 18:21:15 +0300

    11

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    $('.header-burger').click(function (event) {
+        $('.menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
+$(document).ready(function () {
+    $('.header-1').click(function (event) {
+        $('.header-1, .content-1').toggleClass('activee');
+    });
+});
+
+$(document).ready(function () {
+    $('.header-2').click(function (event) {
+        $('.header-2, .content-2').toggleClass('activee');
+    });
+});
+$(document).ready(function () {
+    $('.header-3').click(function (event) {
+        $('.header-3, .content-3').toggleClass('activee');
+    });
+});
+
+$(document).ready(function () {
+    $('.header-4').click(function (event) {
+        $('.header-4, .content-4').toggleClass('activee');
+    });
+});
